07be507590ddc2853798019d94b1d23c5a47c898
Selina <184493135+selinarau@users.noreply.github.com>
2026-05-27 15:08:51 +0000

    fix: fixed the pipeline.py file

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,6 @@
 import sys
 import pandas as pd
 
-
 print("arguments: ", sys.argv)
 
 month = int(sys.argv[1])
@@ -10,6 +9,7 @@ df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df["month"] = month
 print(df.head())
 
-df.to_parquet(f"output/month={month}.parquet")
+df.to_parquet(f"output_{month}.parquet")
+print(f"hello pipeline, month: {month}")
 
  
